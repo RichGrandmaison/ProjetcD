6846dbe9efacd35a863443529f9c308dc815edea
rich <rich.grandmaison@gmail.com>
2015-11-03 15:47:38 -0500

    First commit

---
+++ app/src/main/java/com/rich/projetcd/Trial.java
@@ -0,0 +1,14 @@
+package com.rich.projetcd;
+
+/**
+ * Created by rich on 03/11/15.
+ */
+public class Trial {
+
+    public static String addition(int x, int y) {
+        return String.valueOf(x+y);
+
+
+
+    }
+}
